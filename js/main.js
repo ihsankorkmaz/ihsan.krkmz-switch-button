c2891c2ca4523a9bd8d4455050920ca048264a8f
Graphic <graphic@SOD-SJH-516-11M.local>
2015-10-02 14:44:37 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
 
 
+
+
+$('.switch').on('click', function(e) {
+
+var on = $('button').hasClass('on')
+
+var off = $('button').hasClass('off')
+
+
+if(on == true){
+
+	
+	$('button').addClass('off');
+	
+	$('button').removeClass('on');
+
+	$('body').addClass('dark');
+
+	$('body').removeClass('light');	
+
+	$('.status').text("Hey, who turn off the lights?");
+
+}
+
+if (off == true){
+
+	
+	$('button').addClass('on');
+	
+	$('button').removeClass('off');
+
+	$('body').addClass('light');
+
+	$('body').removeClass('dark');
+
+	$('.status').text(" It's so bright in here! ");
+
+
+
+}
+
+});
